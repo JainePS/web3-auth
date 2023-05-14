e94bf754343a4fe2cc144c875522aa9c5129fabc
Jaine Santos <jaine@valarisolutions.com>
2023-05-14 16:24:35 +0200

    adjust nestconfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {future: {
-    webpack5: false,
-    },}
+const nextConfig = {
+    experimental: { appDir: true },
+    webpack(config) {
+      config.experiments = { ...config.experiments, topLevelAwait: true }
+      return config
+    },
+}
 
 module.exports = nextConfig
